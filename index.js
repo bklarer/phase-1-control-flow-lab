94390402b80309983b56c2e5ab0173034721a8fe
Benjamin Klarer <benjaminklarer@gmail.com>
2022-05-17 21:30:34 -0700

    commit tree 4fb0f67bf96f164f62ecf1ae497d5a8658f9667d

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+let someValue = 199
+
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+    if (someValue <= 400) {
+          return "This one is on me!"
+     }  else if (someValue < 2000) {
+          return "That will be twenty bucks."
+    }   else if (someValue <= 2500) {
+          return "I will gladly take your thirty bucks."
+    }   else {
+          return "No can do."
+    }
+    }
+
+let city
+
+function ternaryCheckCity(city){
+  // Write your code here!
+   return city === "NYC"? "Ok, sounds good." : "No go." ;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+let tip
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip){
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+  }
+
 }
